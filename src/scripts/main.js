bdf02559fbc09e139583729c24d17371ac371b8d
Iryna Polchaninova <ira.polchanin@gmail.com>
2024-10-11 11:10:27 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const listEl = document.querySelector('ul');
+
+const listItemEl = [...document.querySelectorAll('[data-salary]')];
+
+const convertToNumber = (str) => Number(str.replace('$', '').replace(',', ''));
+
+const sortList = (list) =>
+  list.sort(
+    (a, b) =>
+      convertToNumber(b.dataset.salary) - convertToNumber(a.dataset.salary),
+  );
+
+const getEmployees = (sortedList, element) => (element.innerHTML = sortedList);
+
+const visibleEmployeesHTML = sortList(listItemEl)
+  .map(
+    (item) =>
+      `<li
+        data-position="${item.dataset.position}"
+        data-salary="${item.dataset.salary}"
+        data-age="${item.dataset.age}"
+      >
+        ${item.innerText}
+      </li>`,
+  )
+  .join('');
+
+getEmployees(visibleEmployeesHTML, listEl);
